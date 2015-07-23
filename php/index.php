36c8ea82d525e1791dac67d5af7ac69b58124900
AoEiuV <490674483@qq.com>
2015-07-23 21:19:55 +0800

    AoEiuV020 commit...

---
+++ php/index.php
@@ -0,0 +1,24 @@
+<!--***************************************************
+	^> File Name: index.php
+	^> Author: AoEiuV020
+	^> Mail: 490674483@qq.com
+	^> Created Time: 2015/07/23 - 19:11:56
+****************************************************-->
+<!doctype html>
+<head>
+	<meta charset="utf-8" />
+	<title>index.php</title>
+</head>
+<html>
+	<body>
+		<h1>Hello php</h1>
+		<p id="top">php的学习，</p>
+		<p>
+<?php
+echo "hello php...<br/>";
+?>
+		
+		</p>
+	</body>
+</html>
+
