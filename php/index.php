df184320f7c75048a65cdfb4149dfa9c3e452a7b
AoEiuV <490674483@qq.com>
2015-07-25 14:56:55 +0800

    AoEiuV020 commit...

--- php/index.php
+++ php/index.php
@@ -15,9 +15,32 @@
 		<p id="top">php的学习，</p>
 		<p>
 <?php
-echo "hello php...<br/>";
+function li2($href,$inner)
+{
+	echo "<li><a href=\"".$href."\">".$inner."...</a></li>";
+}
+function li($page)
+{
+	li2($page.".php",$page);
+}
+echo "<ol>\n";
+li2("..","go back");
+li2("#bottom","goto bottom");
+li("phpinfo");
+li("hello");
+li("global");
+li("static");
+li("print");
+li("class");
+li("define");
+li("string");
+echo "</ol>\n";
 ?>
 		
+		</p>
+		<p id="bottom">
+		bottom...<br/>
+		<a href="#top">goto top...</a><br/>
 		</p>
 	</body>
 </html>
